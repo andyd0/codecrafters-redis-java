1897505fab1cbd0245c95f12b0d2484de3988add
andyd0 <andy.davila@gmail.com>
2024-11-10 10:16:32 -0500

    Ping pong response

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,4 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -12,7 +12,7 @@ public class Main {
             serverSocket.setReuseAddress(true);
             // Wait for connection from client.
             clientSocket = serverSocket.accept();
-            clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+            processCommand(clientSocket);
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         } finally {
@@ -23,6 +23,26 @@ public class Main {
             } catch (IOException e) {
                 System.out.println("IOException: " + e.getMessage());
             }
+        }
+    }
+
+    private static void processCommand(Socket clientSocket) {
+        try {
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(clientSocket.getOutputStream()));
+
+            String input;
+            while ((input = reader.readLine()) != null) {
+                if (input.toLowerCase().startsWith("ping")) {
+                    writer.write("+PONG\r\n");
+                    writer.flush();
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
