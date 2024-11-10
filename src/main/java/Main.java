5cc8d98be8b520a582accd170bb1684bfb6a85a4
andyd0 <andy.davila@gmail.com>
2024-11-10 10:19:44 -0500

    Add threads

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,53 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args){
         ServerSocket serverSocket = null;
-        Socket clientSocket = null;
         int port = 6379;
+
+        // Using ExecutorService to reuse threads
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
         try {
             serverSocket = new ServerSocket(port);
             serverSocket.setReuseAddress(true);
-            // Wait for connection from client.
-            clientSocket = serverSocket.accept();
-            processCommand(clientSocket);
-        } catch (IOException e) {
-            System.out.println("IOException: " + e.getMessage());
-        } finally {
-            try {
-                if (clientSocket != null) {
-                    clientSocket.close();
+
+            while (!executorService.isShutdown()) {
+                try {
+                    // Wait for connection from client.
+                    final Socket clientSocket = serverSocket.accept();
+                    executorService.execute(() -> {
+                        try {
+                            processCommand(clientSocket);
+                        } finally {
+                            try {
+                                clientSocket.close();
+                            }
+                            catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+                } catch (IOException e) {
+                    if (!executorService.isShutdown()) {
+                        System.err.println("Error accepting client connection: " + e.getMessage());
+                    }
                 }
-            } catch (IOException e) {
-                System.out.println("IOException: " + e.getMessage());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            executorService.shutdown();
+            if (serverSocket != null) {
+                try {
+                    serverSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
